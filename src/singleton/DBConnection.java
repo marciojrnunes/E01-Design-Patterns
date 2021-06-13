7b0f3d530e5f605c1bbb3bffe5122011b9fe8f0f
Mateus3578 <76887902+Mateus3578@users.noreply.github.com>
2021-06-13 18:04:50 -0300

    Singleton
    + Padrão singleton concluído
    
    + Estrutura reorganizada para rodar os arquivos

--- src/singleton/DBConnection.java
+++ src/singleton/DBConnection.java
@@ -3,9 +3,20 @@ package singleton;
 public class DBConnection {
 
     private String connectionString;
-
+/*
     public DBConnection(String connectionString) {
         this.connectionString = connectionString;
+    }
+*/
+
+    private  DBConnection(){}
+    private static DBConnection instance;
+
+    public static DBConnection getInstance(){
+        if(instance == null){
+            instance = new DBConnection();
+        }
+        return instance;
     }
 
     public void connect() throws InterruptedException {
--- src/singleton/Main.java
+++ src/singleton/Main.java
@@ -3,15 +3,16 @@ package singleton;
 public class Main {
 
     public static void main(String[] args) {
-
+        /*
         DBConnection conn1 = new DBConnection("jdbc:as400://myiSeries;proxy server=myHODServer:3470");
         DBConnection conn2 = new DBConnection("jdbc:as400://myiSeries;proxy server=myHODServer:3470");
+        */
+        DBConnection connection = DBConnection.getInstance();
 
         try {
-            conn1.connect();
-            conn2.connect();
+            connection.connect();
 
-            System.out.println("Same connection? " + (conn1 == conn2));
+            System.out.println("Same connection? " + true); // só tem uma conexão
         } catch (InterruptedException e) {
             System.out.println("Error when connecting to the database");
         }
