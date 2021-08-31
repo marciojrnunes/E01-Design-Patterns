ff3191879b5177b3ebaa419bd908f95ad2c197bd
Fernanda de Oliveira Carvalho <nandaoc@pop-os.localdomain>
2021-08-31 13:31:02 -0300

    exercício 1 - singleton

--- src/singleton/DBConnection.java
+++ src/singleton/DBConnection.java
@@ -3,8 +3,10 @@ package singleton;
 public class DBConnection {
 
     private String connectionString;
+    // criando instância da classe
+    private static DBConnection instance;
 
-    public DBConnection(String connectionString) {
+    private DBConnection(String connectionString) {
         this.connectionString = connectionString;
     }
 
@@ -12,5 +14,11 @@ public class DBConnection {
         System.out.println("Connecting to " + this.connectionString);
         Thread.sleep(1000);
         System.out.println("Connected!!");
+    }
+
+    public static DBConnection getInstance(String connectionString) {
+        if (instance == null)
+            instance = new DBConnection(connectionString);
+        return instance;
     }
 }
