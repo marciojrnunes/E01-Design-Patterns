270246081bac67eeaac9b6e946f1fe11dc52bebf
BrendaSales <92118645+BrendaSales@users.noreply.github.com>
2022-05-14 12:25:58 -0300

    Decorator

--- src/main/java/decorator/Main.java
+++ src/main/java/decorator/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Ingrediente produto = new Pao(new Pao());
+        Ingrediente produto = new Pao(new Ketchup(new Queijo(new Bacon(new Salada()))));
 
         System.out.println(produto.imprimeIngrediente());
         System.out.println(produto.valorDoIngrediente());
