41e71b80df85f844f5935a65e44c603c3d272588
pogum <samuel.pogum@gmail.com>
2022-05-15 02:01:20 -0300

    Revert "Revert "E01-Design-Patterns - Padrão Singleton""
    This reverts commit 83886e882cbde0fd90f90b95f35431824d111923.

--- src/main/java/singleton/DBConnection.java
+++ src/main/java/singleton/DBConnection.java
@@ -2,14 +2,27 @@ package singleton;
 
 public class DBConnection {
 
+   private static DBConnection instance;
+
     private String connectionString;
 
-    public DBConnection(String connectionString) {
-        this.connectionString = connectionString;
+    private DBConnection() {
+
     }
 
-    public void connect() throws InterruptedException {
-        System.out.println("Connecting to " + this.connectionString);
+    //public DBConnection(String connectionString) {
+    //    this.connectionString = connectionString;
+   // }
+
+    public static synchronized DBConnection getInstance(String connectionString) {
+        if (instance == null)
+        instance = new DBConnection();
+        instance.connectionString = connectionString;
+        return instance;
+    }
+
+    public void connect(String connectionString) throws InterruptedException {
+        System.out.println("Connecting to " + instance.connectionString);
         Thread.sleep(1000);
         System.out.println("Connected!!");
     }
