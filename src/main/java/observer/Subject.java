b07497ef3856f34d83ef3cecc07e1d71d8af21aa
coltec <coltec@teiacoltec.org>
2022-05-18 20:18:20 -0300

    E01-Design-Patterns - Gabriel

--- src/main/java/observer/Subject.java
+++ src/main/java/observer/Subject.java
@@ -12,11 +12,13 @@ public class Subject {
     }
 
     public void add(Observer observer) {
-//        TODO adicionar observer para ser notificado posteriormente
+        observers.add(observer);
     }
 
     public void notifyObservers() {
-//        TODO notificar observers adicionados
+       for(Observer x: observers){
+           x.update(this);
+       }
     }
 
 }
