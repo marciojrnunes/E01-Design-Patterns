f238ded09b978eb107c85925727d570711f030f2
Arthur Soares <89421140+arthurvns@users.noreply.github.com>
2023-05-04 13:45:39 -0300

    Update DBConnection.java

--- src/main/java/singleton/DBConnection.java
+++ src/main/java/singleton/DBConnection.java
@@ -1,13 +1,18 @@
 package singleton;
 
 public class DBConnection {
-
+    private static DBConnection instance;
     private String connectionString;
 
     public DBConnection(String connectionString) {
         this.connectionString = connectionString;
     }
-
+    public static DBConnection getInstance(String connectionString) {
+        if (instance == null) {
+            instance = new DBConnection(connectionString);
+        }
+        return instance;
+    }
     public void connect() throws InterruptedException {
         System.out.println("Connecting to " + this.connectionString);
         Thread.sleep(1000);
