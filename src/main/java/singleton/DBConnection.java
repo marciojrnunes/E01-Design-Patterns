e85fac2f79b286cb0ffd533a01bfc7356fcb6354
MariGressi <111541073+MariGressi@users.noreply.github.com>
2023-05-05 12:35:15 -0300

    alteração

--- src/main/java/singleton/DBConnection.java
+++ src/main/java/singleton/DBConnection.java
@@ -2,15 +2,34 @@ package singleton;
 
 public class DBConnection {
 
-    private String connectionString;
+    private DBConnection() {}
 
-    public DBConnection(String connectionString) {
-        this.connectionString = connectionString;
+    private static DBConnection instance;
+
+
+    public static DBConnection getInstace() {
+        if(instance == null)
+        instance = new DBConnection();
+        return instance;
+        //this.connectionString = connectionString;
     }
 
-    public void connect() throws InterruptedException {
-        System.out.println("Connecting to " + this.connectionString);
+    public void connect (String connectionString){
+        /*System.out.println("Connecting to " + this.connectionString);
         Thread.sleep(1000);
-        System.out.println("Connected!!");
+        System.out.println("Connected!!");*/
+        System.out.println(connectionString);
+
     }
+
+
+void connection() {
+    DBConnection con = DBConnection.getInstance();
+    con.println("Connecting to "); 
+  }
+  
+  void Connected() {
+    DBConnection con = DBConnection.getInstance();
+    con.println("Connected!!");
+  }
 }
