4046a85c885149857fe78691da38b2b20182eeaf
KarenGiullia <138712215+KarenGiullia@users.noreply.github.com>
2024-04-20 18:19:32 -0300

    Entrega das atividades

--- src/main/java/singleton/DBConnection.java
+++ src/main/java/singleton/DBConnection.java
@@ -2,15 +2,28 @@ package singleton;
 
 public class DBConnection {
 
-    private String connectionString;
+    private static DBConnection instance;
 
     public DBConnection(String connectionString) {
-        this.connectionString = connectionString;
+        private DBConnection(String connectionString) {
+            this.connectionString = connectionString;
+        }
+    
     }
-
+    
+    public static synchronized DBConnection getInstance() {
+        
+        if (instance == null) {
+            // Cria uma nova instância se ainda não existir
+            instance = new DBConnection();
+        }
+        // Retorna a instância existente
+        return instance;
+    }
     public void connect() throws InterruptedException {
         System.out.println("Connecting to " + this.connectionString);
         Thread.sleep(1000);
-        System.out.println("Connected!!");
     }
+   
 }
+
--- src/main/java/strategy/PasswordValidator.java
+++ src/main/java/strategy/PasswordValidator.java
@@ -1,10 +1,16 @@
 package strategy;
 
 public class PasswordValidator {
+    private PasswordStrategy strategy;
 
-    // FIXME só aceita um tipo de validação (por tamanho)
-    public boolean validate(String password) {
-        return password.length() >= 8;
+    public void setStrategy(PasswordStrategy strategy) {
+        this.strategy = strategy;
     }
 
+    public boolean validate(String password) {
+        if (strategy == null) {
+            throw new IllegalStateException("Estratégia de validação não definida");
+        }
+        return strategy.validate(password);
+    }
 }
